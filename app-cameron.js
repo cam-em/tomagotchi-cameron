8b7b4f97e97aef0751fbe59aad343d7db89b14b1
cam-em <cmoreno.1117@gmail.com>
2020-09-23 20:17:14 -0500

    added feed, turnofflights, and play

--- app-cameron.js
+++ app-cameron.js
@@ -8,11 +8,29 @@ class Tomagotchi {
         this.boredom = boredom
     }
 
+    feed() {
+        this.hunger = 1;
+        console.log('Feeding...')
+        console.log(`Hunger: ${this.hunger}`)
+    }
+
+    turnOffLights() {
+        this.sleepiness = 1;
+        console.log('Time for bed!')
+        console.log(`Sleepiness: ${this.sleepiness}`)
+    }
+
+    play() {
+        this.boredom = 1;
+        console.log('It\'s playtime!')
+        console.log(`Boredom: ${this.boredom}`)
+    }
+
 }
 
+const obj = new Tomagotchi(10);
 
-document.addEventListener('DOMContentLoaded', ()=>{
-    const obj = new Tomagotchi(10);
+const displayMetrics = (obj) => {
     const metrics = Object.keys(obj);
     for (metric of metrics) {
         //Makes the first letter of the key of Tomagatchi upper case
@@ -20,4 +38,9 @@ document.addEventListener('DOMContentLoaded', ()=>{
         //Logs all keys and values of Tomagotchi
         console.log(`${upperCasemetric}: ${obj[metric]}`);
     }
+}
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    displayMetrics(obj);
+
 })
